5693e353696a65608ab7914c2aa6c32daf23897d
Ana <ana.eng.dev@gmail.com>
2020-07-22 18:02:59 -0300

    layout ok

---
+++ src/components/Pill.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="pill">
+    <span class="name">
+      {{ name }}
+    </span>
+    <span class="amount">
+      {{ amount }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"],
+};
+</script>
+
+<style scoped>
+.pill {
+  background-color: rgb(165, 42, 79);
+  color: aliceblue;
+  margin: 10px;
+  height: 55px;
+  border-radius: 30px;
+  font-size: 1.6rem;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+}
+.pill .name {
+  margin-left: 15px;
+}
+.pill .amount {
+  margin: 10px;
+  padding: 5px;
+  height: 35px;
+  width: 35px;
+  border-radius: 18px;
+
+  background-color: aliceblue;
+  color: black;
+
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
